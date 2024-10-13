c1c0516e843800fb49c1a130996a5872849b89c0
janmi <janmischkies@web.de>
2024-10-13 17:01:17 +0200

    New Collection Modification Endpoints

--- src/main/java/de/swa/gmaf/api/GMAF_Facade_RESTImpl.java
+++ src/main/java/de/swa/gmaf/api/GMAF_Facade_RESTImpl.java
@@ -62,43 +62,6 @@ public class GMAF_Facade_RESTImpl extends ResourceConfig {
 		return uuid;
 	}
 
-
-	/**
-	 * returns a Graph Code for a given MMFG
-	 **/
-	@POST
-	@Path("/getgc/{auth-token}/{mmfg_id}")
-	@Produces("application/json")
-	public String getOrGenerateGraphCode(@PathParam("auth-token") String auth_token, @PathParam("mmfg_id") String mmfg_id) {
-		System.out.println("getGc " + mmfg_id);
-		MMFGCollection coll = MMFGCollection.getInstance(auth_token);
-		UUID id = UUID.fromString(mmfg_id);
-		MMFG mmfg = coll.getMMFGForId(id);
-		GraphCode gc = GraphCodeGenerator.generate(mmfg);
-		String json = GraphCodeIO.asJson(gc);
-		System.out.println("-> " + json);
-		return json;
-	}
-
-	/**
-	 * returns MMFG for id
-	 **/
-	@POST
-	@Path("/getmmfg/{auth-token}/{mmfg-id}")
-	@Produces("application/json" + ";charset=utf-8")
-	public Response getMMFGForId(@PathParam("auth-token") String auth_token, @PathParam("mmfg-id") String mmfg_id) {
-		System.out.println("getMMFG " + mmfg_id);
-		MMFGCollection coll = MMFGCollection.getInstance(auth_token);
-		UUID id = UUID.fromString(mmfg_id);
-		MMFG m = coll.getMMFGForId(id);
-		if (m == null) {
-			System.out.println("-> not found");
-			return Response.status(Response.Status.NOT_FOUND).build();
-		}
-		System.out.println("-> " + m.getId() + " with " + m.getAllNodes().size() + " nodes");
-		return Response.ok(m).build();
-	}
-
 	// data structures to hold sessions
 	private Hashtable<String, GMAF> sessions = new Hashtable<String, GMAF>();
 	private Hashtable<String, String> errorMessages = new Hashtable<String, String>();
@@ -112,92 +75,6 @@ public class GMAF_Facade_RESTImpl extends ResourceConfig {
 		else throw new RuntimeException("no valid API key");
 	}
 
-	/**
-	/**
-	 * returns the collection of MMFGs for a given auth_token
-	 **/
-	@POST
-	@Path("/getCollection/{auth-token}")
-	@Produces("application/json")
-	@WebMethod
-	public Vector<MMFG> getCollection(@PathParam("auth-token") String auth_token) {
-		MMFGCollection coll = MMFGCollection.getInstance(auth_token);
-		return coll.getCollection();
-	}
-
-	/**
-	 * returns an image with given id
-	 **/
-	@GET
-	@Path("/preview/{auth-token}/{id}")
-	@Produces("image/*")
-	@WebMethod
-	public Response getImage(@PathParam("auth-token") String auth_token, @PathParam("id") String mmfg_id) {
-		System.out.println("preview " + mmfg_id);
-		MMFGCollection coll = MMFGCollection.getInstance(auth_token);
-		UUID id = UUID.fromString(mmfg_id);
-		MMFG mmfg = coll.getMMFGForId(id);
-		// Build and return a response with the provided image
-		File file = mmfg.getGeneralMetadata().getFileReference();
-		System.out.println("-> " + file.getAbsolutePath());
-		String type = "undefined";
-		try {
-			//type = new MimetypesFileTypeMap().getContentType(file);
-			type = Files.probeContentType(file.toPath());
-
-		} catch (Throwable t) {
-			System.out.println("-> " + t.getMessage());
-		}
-		System.out.println("--> " + type);
-		Response res = Response.ok().entity(file).type(type).build();
-		System.out.println("---> " + res.getHeaderString("contentType"));
-		return res;
-	}
-
-	/**
-	 * returns image-URL as String
-	 **/
-	@POST
-	@Path("/preview/{auth-token}/{id}")
-	@Produces("application/json")
-	@WebMethod
-	public String getPreviewURL(@PathParam("auth-token") String auth_token, @PathParam("id") String mmfg_id) {
-		MMFGCollection coll = MMFGCollection.getInstance(auth_token);
-		UUID id = UUID.fromString(mmfg_id);
-		MMFG mmfg = coll.getMMFGForId(id);
-		return mmfg.getGeneralMetadata().getPreviewUrl().toString();
-	}
-
-
-	/**
-	 * returns a list of similar assets for a given Graph Code
-	 **/
-	@POST
-	@Path("/getSim/{auth-token}/{id}")
-	@Produces("application/json")
-	@WebMethod
-	public Vector<MMFG> getSimilarAssets(@PathParam("auth-token") String auth_token, @PathParam("id") String mmfg_id) {
-		MMFGCollection coll = MMFGCollection.getInstance(auth_token);
-		UUID id = UUID.fromString(mmfg_id);
-		MMFG mmfg = coll.getMMFGForId(id);
-		GraphCode gc = GraphCodeGenerator.generate(mmfg);
-		return coll.getSimilarAssets(gc);
-	}
-
-	/**
-	 * returns a list of recommendations for a given Graph Code
-	 **/
-	@POST
-	@Path("/getRec/{auth-token}/{id}")
-	@Produces("application/json")
-	@WebMethod
-	public Vector<MMFG> getRecommendedAssets(@PathParam("auth-token") String auth_token, @PathParam("id") String mmfg_id) {
-		MMFGCollection coll = MMFGCollection.getInstance(auth_token);
-		UUID id = UUID.fromString(mmfg_id);
-		MMFG mmfg = coll.getMMFGForId(id);
-		GraphCode gc = GraphCodeGenerator.generate(mmfg);
-		return coll.getRecommendedAssets(gc);
-	}
 
 	@POST
 	@Path("/query/{auth-token}/{query}")
@@ -244,66 +121,92 @@ public class GMAF_Facade_RESTImpl extends ResourceConfig {
 
 
 	/**
-	 * Make a multimedia Query
+	 * adds item to the collection
 	 **/
 	@POST
-	@Path("/queryMultimedia/{auth-token}")
+	@Path("/addItem/{auth-token}")
 	@Produces("application/json")
 	@WebMethod
-	public String queryMultiMedia(String filejson, @PathParam("auth-token") String auth_token) {
-
-		try {
+	public MMFG addItem(String filejson, @PathParam("auth-token") String auth_token) {
 
 		JSONObject myjson = new JSONObject(filejson);
-		String text = myjson.getString("text");
+		String base64string = myjson.getString("file");
+		String name = myjson.getString("name");
+		boolean overwrite = myjson.getBoolean("overwrite");
+		String collectionPath = Configuration.getInstance().getCollectionPaths().get(0);
+		String storePath = collectionPath + "/" + name;
 
-		System.out.println(text);
-
-			String name = myjson.getString("name");
-		}catch (Throwable t) {
-			t.printStackTrace();
+		System.out.println(storePath);
+		File file = new File(storePath);
+		if (!overwrite) {
+			if (file.exists()) {
+				System.out.println("File already exists");
+				return null;
+			}
 		}
-		/*
-		System.out.println("HIER");
-		GraphCode gc = new GraphCode();
-		Vector<String> dict = new Vector<String>();
-		keywords = keywords.replace(";", ",");
-		//keywords = keywords.replace(" ", ",");
-		String[] str = keywords.split(",");
-		for (String s : str) dict.add(s.trim());
-		gc.setDictionary(dict);
+		// Create a File object representing the file to write
+		byte[] contentBytes = Base64.getDecoder().decode(base64string);
+		// Use try-with-resources to ensure the stream is closed properly
+		try (FileOutputStream fos = new FileOutputStream(file)) {
+			fos.write(contentBytes);
+			System.out.println("File written successfully to " + storePath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-		System.out.println("query by keyword " + keywords + " with token " + auth_token);
-		*/
-		/*
+
 		try {
-
-			Vector<String> ids = new Vector<String>();
+			File f = new File(storePath);
+			GMAF gmaf = new GMAF();
+			MMFG fv = gmaf.processAsset(f);
 			MMFGCollection coll = MMFGCollection.getInstance(auth_token);
-			for (MMFG m : coll.getSimilarAssets(gc)) {
-				ids.add(m.getId().toString());
-			}
-
-			if (ids.size() == 0) {
-				Vector<MMFG> mmfgs = MMFGCollection.getInstance(auth_token).getCollection();
-				for (MMFG m : mmfgs) ids.add(m.getId().toString());
-			}
-
-			System.out.println("found " + ids.size() + " results");
-
-			String[] strx = new String[ids.size()];
-			for (int i = 0; i < strx.length; i++) {
-				String s = ids.get(i);
-				strx[i] = s;
-			}
-			return strx;
+			coll.addToCollection(fv);
+			return fv;
 		} catch (Exception e) {
 			e.printStackTrace();
-		}	 */
+		}
 
-		String res=  "{\"result\" :\"finsisehd\"}";
-		return  res;
 
+		MMFG mmfg = new MMFG();
+		MMFGCollection coll = MMFGCollection.getInstance(auth_token);
+		//coll.addToCollection();
+
+		return null;
+	}
+
+	/**
+	 * delete items from the collection
+	 **/
+	@POST
+	@Path("/deleteItem/{auth-token}/{itemid}")
+	@Produces("application/json")
+	@WebMethod
+	public boolean deleteItem(@PathParam("itemid") String itemid, @PathParam("auth-token") String auth_token) {
+
+		try {
+			MMFGCollection coll = MMFGCollection.getInstance(auth_token);
+			Vector<MMFG> v = coll.getCollection();
+			for (MMFG mmfg : v) {
+				JSONObject jsonObject = new JSONObject(mmfg.getGeneralMetadata());
+				if (jsonObject.getString("id").equals(itemid)) {
+					File f = mmfg.getGeneralMetadata().getFileReference();
+					String filename = mmfg.getGeneralMetadata().getFileName();
+
+					if (f.exists()) {
+						//f.delete();
+						System.out.println("File deleted successfully at: " + f.getPath());
+						return true;
+					}
+				}
+			}
+		} catch (Throwable x) {
+
+			x.printStackTrace();
+			errorMessages.put(auth_token, x.getMessage());
+			return false;
+		}
+
+		return false;
 	}
 
 	@POST
@@ -322,24 +225,61 @@ public class GMAF_Facade_RESTImpl extends ResourceConfig {
 		return str;
 	}
 
+
 	/**
-	 * returns Metadata for collection items
+	 * Make a query with mmco
 	 **/
 	@POST
-	@Path("/getMetadata/{auth-token}")
+	@Path("/getQueryIds/{auth-token}")
 	@Produces("application/json")
 	@WebMethod
-	public GeneralMetadata[] getCollectionMetadata(@PathParam("auth-token") String auth_token) {
+	public String getQueryIds(String cmmcoJson, @PathParam("auth-token") String auth_token) {
+
+		try {
+
+
+			JSONObject cmmco = new JSONObject(cmmcoJson);
+
+			JSONObject mmco= cmmco.getJSONObject("MMCO");
+			JSONObject image = mmco.getJSONObject("image");
+			String name = image.getString("name");
+			//String file= image.getString("file");
+
+			System.out.println(name);
+			//System.out.println(file.substring(0,500));
+			String md = cmmco.getString("MD");
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+
+		String res = "{\"result\" :\"finsisehd\"}";
+		return res;
+
+	}
+
+	/**
+	 * returns Metadata for specific collection items
+	 **/
+	@POST
+	@Path("/getCmmco/{auth-token}/{itemid}")
+	@Produces("application/json")
+	@WebMethod
+	public String getCmmco(@PathParam("auth-token") String auth_token, @PathParam("itemid") String itemid) {
 
 		MMFGCollection coll = MMFGCollection.getInstance(auth_token);
 		Vector<MMFG> v = coll.getCollection();
-		GeneralMetadata[] str = new GeneralMetadata[v.size()];
-		for (int i = 0; i < v.size(); i++) {
-			str[i] = v.get(i).getGeneralMetadata();
-		}
 
-		return str;
+		for (MMFG mmfg : v) {
+			JSONObject jsonObject = new JSONObject(mmfg.getGeneralMetadata());
+			if (jsonObject.getString("id").equals(itemid)) {
+
+				return "Return1";
+			}
+		}
+		return "Return2";
 	}
+
 
 	/**
 	 * returns Metadata for specific collection items
@@ -352,7 +292,7 @@ public class GMAF_Facade_RESTImpl extends ResourceConfig {
 
 		MMFGCollection coll = MMFGCollection.getInstance(auth_token);
 		Vector<MMFG> v = coll.getCollection();
-        for (MMFG mmfg : v) {
+		for (MMFG mmfg : v) {
 			JSONObject jsonObject = new JSONObject(mmfg.getGeneralMetadata());
 			if (jsonObject.getString("id").equals(itemid)) {
 
@@ -405,269 +345,11 @@ public class GMAF_Facade_RESTImpl extends ResourceConfig {
 				}
 			}
 
-		}
-		catch (Throwable x) {
+		} catch (Throwable x) {
 			x.printStackTrace();
 			errorMessages.put(auth_token, x.getMessage());
 		}
 		return null;
-	}
-
-	/**
-	 * adds item to the collection
-	 **/
-	@POST
-	@Path("/addItem/{auth-token}")
-	@Produces("application/json")
-	@WebMethod
-	public MMFG addItem( String filejson, @PathParam("auth-token") String auth_token) {
-
-		JSONObject myjson = new JSONObject(filejson);
-		String base64string = myjson.getString("file");
-		String name = myjson.getString("name");
-		boolean overwrite = myjson.getBoolean("overwrite");
-		String collectionPath= Configuration.getInstance().getCollectionPaths().get(0);
-		String storePath= collectionPath+"/"+name;
-
-		System.out.println(storePath);
-		File file = new File(storePath);
-		if(!overwrite){
-			if(file.exists()){
-				System.out.println("File already exists");
-				return null;
-			}
-		}
-		// Create a File object representing the file to write
-		byte[] contentBytes = Base64.getDecoder().decode(base64string);
-		// Use try-with-resources to ensure the stream is closed properly
-		try (FileOutputStream fos = new FileOutputStream(file)) {
-			fos.write(contentBytes);
-			System.out.println("File written successfully to "+ storePath);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-
-		try{
-			File f= new File(storePath);
-			GMAF gmaf = new GMAF();
-			MMFG fv = gmaf.processAsset(f);
-			MMFGCollection coll = MMFGCollection.getInstance(auth_token);
-			coll.addToCollection(fv);
-			return fv;
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-
-
-		MMFG mmfg = new MMFG();
-		MMFGCollection coll = MMFGCollection.getInstance(auth_token);
-		//coll.addToCollection();
-
-		return null;
-	}
-
-
-	/**
-	 * delete items from the collection
-	 **/
-	@POST
-	@Path("/deleteItem/{auth-token}/{itemid}")
-	@Produces("application/json")
-	@WebMethod
-	public boolean deleteItem(@PathParam("itemid") String itemid , @PathParam("auth-token") String auth_token) {
-
-		try {
-			MMFGCollection coll = MMFGCollection.getInstance(auth_token);
-			Vector<MMFG> v = coll.getCollection();
-			for (MMFG mmfg : v) {
-				JSONObject jsonObject = new JSONObject(mmfg.getGeneralMetadata());
-				if (jsonObject.getString("id").equals(itemid)) {
-					File f = mmfg.getGeneralMetadata().getFileReference();
-					String filename= mmfg.getGeneralMetadata().getFileName();
-
-					if(f.exists()){
-						//f.delete();
-						System.out.println("File deleted successfully at: "+f.getPath());
-						return true;
-					}
-				}
-			}
-		}
-		catch (Throwable x) {
-
-			x.printStackTrace();
-			errorMessages.put(auth_token, x.getMessage());
-			return false;
-		}
-
-		return false;
-	}
-
-
-
-	/*
-	 *//** processes an asset with the GMAF Core and returns the calculated MMFG **//*
-	public MMFG processAsset(String auth_token, @FormParam("file") File f) {
-		try {
-			return getSession(auth_token).processAsset(f);
-		}
-		catch (Exception x) {
-			x.printStackTrace();
-			errorMessages.put(auth_token, x.getMessage());
-		}
-		return null;
-	}
-
-	*//** processes an asset with the GMAF Core and returns the calculated MMFG **//*
-	@WebMethod public MMFG processAsset(String auth_token, byte[] bytes, String suffix) {
-		try {
-			File f = File.createTempFile("gmaf", suffix);
-			FileOutputStream fout = new FileOutputStream(f);
-			fout.write(bytes);
-			fout.flush();
-			fout.close();
-			return getSession(auth_token).processAsset(f);
-		}
-		catch (Exception x) {
-			x.printStackTrace();
-			errorMessages.put(auth_token, x.getMessage());
-		}
-		return null;
-	}
-
-	*//** processes an asset with the GMAF Core and returns the calculated MMFG **//*
-	@POST
-    @Path("/{process-asset}/{mmfg}")
-	@Produces("application/json")
-	public MMFG processAsset(@PathParam("auth-token") String auth_token, @PathParam("url") String surl) {
-		try {
-			URL url = new URL(surl);
-			URLConnection uc = url.openConnection();
-			byte[] bytes = uc.getInputStream().readAllBytes();
-			String suffix = url.toString();
-			suffix = suffix.substring(suffix.lastIndexOf(".") + 1, suffix.length());
-			return processAsset(auth_token, bytes, suffix);
-		}
-		catch (Exception x) {
-			x.printStackTrace();
-			errorMessages.put(auth_token, x.getMessage());
-		}
-		return null;
-	}
-
-	*/
-
-	/**
-	 * sets the classes of the processing plugins (optional)
-	 **//*
-	@WebMethod public void setProcessingPlugins(String auth_token, Vector<String> plugins) {
-		getSession(auth_token).setProcessingPlugins(plugins);
-	}                                                                      */
-	                                 /*
-	@POST
-	@Path("/query-by-example")
-	@Produces("application/json")
-	@WebMethod public String[] queryByExample(@PathParam("auth-token") String auth_token, @PathParam("mmfg-id") String mmfg_id) {
-		QueryByExampleCommand qbe = new QueryByExampleCommand(mmfg_id, auth_token);
-		qbe.execute();
-		return getCollectionIds(auth_token);
-	}
-
-	@POST
-	@Path("/{query-by-sparql}")
-	@Produces("application/json")
-	@WebMethod public String[] queryBySPARQL(@PathParam("auth-token") String auth_token, @PathParam("query") String query) {
-		QueryBySPARQLCommand qbs = new QueryBySPARQLCommand(query);
-		qbs.setSessionId(auth_token);
-		qbs.execute();
-		return getCollectionIds(auth_token);
-	}
-
-	@POST
-    @Path("/{get-last-error}")
-	@Produces("application/json")
-	@WebMethod public String getLastError(@PathParam("auth-token") String auth_token) {
-		return errorMessages.get(auth_token);
-	}*/
-	@POST
-	@Path("/test")
-	@Produces("application/json")
-	@WebMethod
-	public String getTest() {
-		System.out.println("Called Test, return stuff");
-		return "test";
-	}
-
-
-	@POST
-	@Path("/getsimilarassetsbygraphcode/{auth-token}")
-	@Produces("application/json")
-	@WebMethod
-	public Vector<String> getSimilarAssetIDsByGraphCode(@PathParam("auth-token") String auth_token, String x) {
-		Gson gson = new Gson();
-		GraphCode gc = gson.fromJson(x, GraphCode.class);
-
-		Vector<String> ids = new Vector<String>();
-		MMFGCollection coll = MMFGCollection.getInstance(auth_token);
-		for (MMFG m : coll.getSimilarAssets(gc)) {
-			ids.add(m.getId().toString());
-		}
-		return ids;
-	}
-
-	@POST
-	@Path("/sparql/{auth-token}/")
-	@Consumes(MediaType.TEXT_PLAIN)
-	@Produces(MediaType.APPLICATION_JSON)
-	@WebMethod
-	public String[] queryBySparQL(@PathParam("auth-token") String auth_token, String query) {
-		Query q = QueryFactory.create(query);
-
-		QueryExecution qexec = QueryExecutionFactory.create(q, MMFGCollection.getInstance().getRDFModel());
-
-		ResultSet results = qexec.execSelect();
-		int colNum = results.getResultVars().size();
-		int rowNum = 0;
-		String[] header = new String[colNum];
-		results.getResultVars().toArray(header);
-		ArrayList<ArrayList> tempData = new ArrayList<ArrayList>();
-
-		for (; results.hasNext(); ) {
-			QuerySolution soln = results.nextSolution();
-			ArrayList row = new ArrayList();
-			for (int i = 0; i < colNum; i++) {
-				row.add(soln.get(header[i]));
-			}
-			tempData.add(row);
-			rowNum++;
-
-		}
-		Object[][] data = new Object[rowNum][colNum];
-
-		String[] str = new String[rowNum];
-		for (int i = 0; i < rowNum; i++) {
-			for (int j = 0; j < colNum; j++) {
-				//data[i][j] = tempData.get(i).get(j)
-				System.out.println(tempData.get(i).get(j));
-				String f = tempData.get(i).get(j).toString();
-				String[] xf = f.split("/");
-				String fname = xf[xf.length - 1];
-				System.out.println(fname);
-
-				File file = new File(Configuration.getInstance().getCollectionPaths().elementAt(0) + "/" + fname);
-				MMFG mmfg = MMFGCollection.getInstance().getMMFGForFile(
-						file);
-				System.out.println(mmfg.getId());
-				str[i] = mmfg.getId().toString();
-			}
-
-		}
-
-		qexec.close();
-
-		return str;
-
 	}
 
 }
